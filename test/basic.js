6d0c01e0aac04cc53054ff11687f4552765f76b7
Neil Kandalgaonkar <neilk@brevity.org>
2013-01-21 21:57:16 -0800

    added callback test

--- test/basic.js
+++ test/basic.js
@@ -9,3 +9,16 @@ test("basic", function(t) {
   t.equal(add(10, 20), m_add(10, 20));
   t.end();
 });
+
+test("with callback", function(t) {
+  function add(a, b) {
+    return a + b;
+  }
+  var normalResult = add(11, 22);
+  function cb(result) {
+    t.equal(normalResult, result);
+  }
+  var m_add = memoize(add, { next: cb });
+  m_add(11, 22);
+  t.end();
+});
